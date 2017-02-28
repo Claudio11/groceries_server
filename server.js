ff66d29ab0484c4dc79287b78a38676e6e8d14d1
Claudio Soto <csoto@altimetrik.com>
2017-02-28 16:10:34 -0500

    First draft for socket connection.

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ import mongoose from 'mongoose';
 import routes from './routes';
 import bodyParser from 'body-parser';
 
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+io.on('connection', function(client){
+  // client.on('event', function(data){});
+  // client.on('disconnect', function(){});
+  client.emit('message', {type:'new-message', text: 'tat'});
+  console.info('asdasd');
+
+  client.on('from-client', function (data) {
+    console.log('data ata', data);
+  });
+});
+
 app.use( (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -39,4 +54,9 @@ app.use('/api', routes);
 let port = process.env.PORT || 4000;
 app.listen(port, '0.0.0.0', () => {
   console.log('Server started at port number: ', port);
+});
+
+server.listen(5000, () => {
+  console.log('started on port 5000');
+
 });
