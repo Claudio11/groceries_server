d28d43ec38007d75274ce4e84e26f4f7052fc5ec
Juan Porley <jporley@altimetrik.com>
2017-03-10 16:43:21 -0300

    added initial data emit

--- helpers/collaboration.js
+++ helpers/collaboration.js
@@ -28,15 +28,15 @@ let sendCurrentHashOnInvalid = (client) => {
  */
 let createAnnotationsResponse = () => {
   return new Promise((resolve, reject) => {
-      Annotation.find({}).populate('author').exec(function (err, list) {
-        if (err) {
-            reject({ success: false, error: err, annotations: list });
-        }
-        else {
-            resolve({ success: true, annotations: list, currentHash: setCurrentHash() });
-        }
+    Annotation.find({}).populate('author').exec(function (err, list) {
+      if (err) {
+        reject({ success: false, error: err, annotations: list });
+      }
+      else {
+        resolve({ success: true, annotations: list, currentHash: setCurrentHash() });
+      }
     });
-  });  
+  });
 }
 
 let sendError = (err) => {
@@ -55,69 +55,72 @@ setCurrentHash();
 let annotationsHelper = {
 
   bindAnnotationsClientEvents: (io, client) => {
-      client.emit('annotation-current-hash', { currentHash: currentHash });
+    // emit initial list and hash
+    createAnnotationsResponse().then(data => {
+      io.in('collaboration').emit('remote-annotations', data);
+    });
 
-      // Client created a new annotation.
-      client.on('local-annotation-create', function (data) {
-        if (data.currentHash === currentHash) {
-            let item = new Annotation(data.annotation);
-            item.save((err) => {
-              if (err) {
-                sendError(err);
-              }
-              else {
-                createAnnotationsResponse().then(data => {
-                  io.in('collaboration').emit('remote-annotations', data);
-                });
-              }
+    // Client created a new annotation.
+    client.on('local-annotation-create', function (data) {
+      if (data.currentHash === currentHash) {
+        let item = new Annotation(data.annotation);
+        item.save((err) => {
+          if (err) {
+            sendError(err);
+          }
+          else {
+            createAnnotationsResponse().then(data => {
+              io.in('collaboration').emit('remote-annotations', data);
             });
-        }
-        else {
-            sendCurrentHashOnInvalid(client);
-        }
-      });
+          }
+        });
+      }
+      else {
+        sendCurrentHashOnInvalid(client);
+      }
+    });
 
-      // Client edited an annotation.
-      client.on('local-annotation-edit', function (data) {
-        if (data.currentHash === currentHash) {
+    // Client edited an annotation.
+    client.on('local-annotation-edit', function (data) {
+      if (data.currentHash === currentHash) {
+        io.in('collaboration').emit('remote-annotations', createAnnotationsResponse());
+      }
+      else {
+        sendCurrentHashOnInvalid(client);
+      }
+    });
+
+    // Client deleted an annotation.
+    client.on('local-annotation-delete', function (data) {
+      if (data.currentHash === currentHash) {
+        Annotation.remove({ _id: data.annotation.id }, function (err) {
+          if (err) {
+            sendError(err);
+          }
+          else {
             io.in('collaboration').emit('remote-annotations', createAnnotationsResponse());
-        }
-        else {
-            sendCurrentHashOnInvalid(client);
-        }
-      });
+          }
+        });
+      }
+      else {
+        sendCurrentHashOnInvalid(client);
+      }
+    });
 
-      // Client deleted an annotation.
-      client.on('local-annotation-delete', function (data) {
-        if (data.currentHash === currentHash) {
-          Annotation.remove( { _id: data.annotation.id }, function (err) {
-              if (err) {
-                  sendError(err);
-              }
-              else {
-                  io.in('collaboration').emit('remote-annotations', createAnnotationsResponse());
-              }
-          });
-        }
-        else {
-            sendCurrentHashOnInvalid(client);
-        }
-      });
+    // Client locked an annotation.
+    client.on('local-annotation-lock', function (data) {
+      if (data.currentHash === currentHash) {
+        let response = { lockedAnnotation: data.id, currentHash: setCurrentHash() };
+        io.in('collaboration').emit('remote-annotations-lock', response);
+      }
+      else {
+        sendCurrentHashOnInvalid(client);
+      }
+    });
 
-      // Client locked an annotation.
-      client.on('local-annotation-lock', function (data) {
-        if (data.currentHash === currentHash) {
-            let response = { lockedAnnotation: data.id, currentHash: setCurrentHash() };
-            io.in('collaboration').emit('remote-annotations-lock', response);
-        }
-        else {
-            sendCurrentHashOnInvalid(client);
-        }
-      });
-
-      // client.on('disconnect', function(client){
-      //   io.in('collaboration').emit('client-disconnected', {clientId: client.id});
-      // });
+    // client.on('disconnect', function(client){
+    //   io.in('collaboration').emit('client-disconnected', {clientId: client.id});
+    // });
   }
 
   // bindAnnotations: (app) => {
@@ -130,7 +133,7 @@ let annotationsHelper = {
 
   //     client.join('collaboration');
   //     client.emit('set-id', { clientId: client.id });
-      
+
 
 
   //   });
